7dbcd341656ab6766085c2c00e6ebe8b29538405
leo <34465410+DrAtomic@users.noreply.github.com>
2020-03-24 21:32:33 -0700

    Update email.py

--- email.py
+++ email.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
-
+import pandas as pd
 import csv, smtplib, ssl
+from email.message import EmailMessage
 
-message = """Subject: subject here
-message here
-"""
-from_address = "email here"
-password = 'password here'
+def email(df,subject,body,sender,password):
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(from_address, password)
-    with open("dataWithEmail.csv") as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip header row
-        for email in reader:
-            try:
-                server.sendmail(
-                    from_address,
-                    email,
-                    message)
-            except:
-                pass
+    context = ssl.create_default_context()
 
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+
+        server.login(from_address, password)
+        try:
+            for email in df['email']:
+                msg = EmailMessage()
+                msg['Subject'] = subject
+                msg['From'] = sender
+                msg.set_content(body)
+                msg['to'] = email
+                server.send_message(msg)
+        except:
+            pass
+
+if __name__ == "__main__":
+    df = pd.read_csv('file')
+    subject = 'test'
+    body = 'this is a test'
+    sender = 'sender email here'
+    password = 'password to sender email here'
+    email(df,subject,body,sender,password)
+    print('done')
